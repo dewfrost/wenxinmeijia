e1bc30be4925b4daf7f80b90c85a94d7d56d1c69
yan <1581931375@qq.com>
2018-03-30 20:57:29 +0800

    'modifyzyy'

--- src/page/add-address.vue
+++ src/page/add-address.vue
@@ -155,9 +155,11 @@ export default {
         this.$refs.textArea.style['-moz-transform'] = 'scale(' + (1 / slace) + ')';
         this.$refs.textArea.style['-webkit-transform'] = 'scale(' + (1 / slace) + ')';
         this.$refs.textArea.style['-o-transform'] = 'scale(' + (1 / slace) + ')';
-        this.$refs.textArea.style('padding', '20');
+        this.$refs.textArea.style('padding', '0 20');
+        this.$refs.textArea.style.paddingLeft = 20 +'px';
+        this.$refs.textArea.style.paddingRight = 20 +'px';
       } else {
-        this.$refs.textArea.style('fontSize', '24');
+        this.$refs.textArea.style.fontSize = 24 +'px';
       }
     }
   }
